e1ffe8af02bdf2f45a37b906433178bd3644451d
Gabriel Rezende <122244605+rznd3@users.noreply.github.com>
2024-06-26 09:32:40 -0300

    Update 4.sql

--- Caso4-Consultas-SQL/4.sql
+++ Caso4-Consultas-SQL/4.sql
@@ -1,2 +1,6 @@
--- Deleta os registros do vendedor de id 2 nas tabelas "Vendedor" e "Venda". ON DELETE CASCADE configurado na chave estrangeira.
-DELETE FROM Vendedor WHERE id = 2
+-- Se a opção "ON DELETE CASCADE" não estiver configurada, será necessário primeiro excluir os dados do vendedor de ID 2 da tabela "Vendedor" e, em seguida, os registros correspondentes na tabela "Venda"
+DELETE FROM Vendedor WHERE id = 2;
+DELETE FROM Venda WHERE vendedor_id = 2;
+
+-- As chaves estrangeiras com a cláusula ON DELETE CASCADE na tabela Venda garantem que todas as vendas associadas ao vendedor serão deletadas automaticamente quando o vendedor for removido.
+-- DELETE FROM Vendedor WHERE id = 2;
